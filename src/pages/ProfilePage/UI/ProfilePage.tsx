52fadbc8609ab03174ab456423fd4cd3a5837b08
OlyaSpiridonova <Spiridonova.olya14@gmail.com>
2023-09-27 13:30:53 +0700

    add profile page, memo, update sidebar

---
+++ src/pages/ProfilePage/UI/ProfilePage.tsx
@@ -0,0 +1,27 @@
+import { profileReducer } from 'entities/Profile';
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+// import style from './ProfilePage.module.scss';
+
+const reducers: ReducersList = {
+    profile: profileReducer,
+};
+
+interface ProfilePageProps {
+    className?: string;
+}
+
+const ProfilePage = () => {
+    const { t } = useTranslation();
+
+    return (
+        <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
+            <div className={classNames('')}>
+                {t('Profile Page')}
+            </div>
+        </DynamicModuleLoader>
+    );
+};
+
+export default ProfilePage;
