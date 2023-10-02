0185e3486609cca468ed02ea971913ee16ea9ac0
OlyaSpiridonova <Spiridonova.olya14@gmail.com>
2023-10-02 10:37:43 +0700

    add profileCard cmp, fetching profile data

--- src/pages/ProfilePage/UI/ProfilePage.tsx
+++ src/pages/ProfilePage/UI/ProfilePage.tsx
@@ -1,7 +1,9 @@
-import { profileReducer } from 'entities/Profile';
+import { ProfileCard, fetchProfileData, profileReducer } from 'entities/Profile';
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 // import style from './ProfilePage.module.scss';
 
 const reducers: ReducersList = {
@@ -14,11 +16,16 @@ interface ProfilePageProps {
 
 const ProfilePage = () => {
     const { t } = useTranslation();
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(fetchProfileData());
+    }, [dispatch]);
 
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
             <div className={classNames('')}>
-                {t('Profile Page')}
+                <ProfileCard />
             </div>
         </DynamicModuleLoader>
     );
