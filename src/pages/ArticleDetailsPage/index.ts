34bd36ba8b6f1dc6c99167e16837733f036c06d9
OlyaSpiridonova <Spiridonova.olya14@gmail.com>
2023-10-17 16:57:20 +0700

    add comment list for articles

--- src/pages/ArticleDetailsPage/index.ts
+++ src/pages/ArticleDetailsPage/index.ts
@@ -1,3 +1,5 @@
 export {
     ArticleDetailsPageAsync as ArticleDetailsPage,
 } from './UI/ArticleDetailsPage.async';
+
+export { ArticleDetailsCommentSchema } from './model/types/ArticleDetailsCommentSchema';
