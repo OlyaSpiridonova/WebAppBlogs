c1e5623a65d43349abd6e4bb72bd7ed0b2411115
OlyaSpiridonova <Spiridonova.olya14@gmail.com>
2023-11-18 18:40:33 +0700

    add private route with required roles

---
+++ src/pages/AdminPanelPage/index.ts
@@ -0,0 +1,5 @@
+import { AdminPanelPageAsync } from './UI/AdminPanelPage.async';
+
+export {
+    AdminPanelPageAsync as AdminPanelPage,
+};
---
+++ src/pages/ForbiddenPage/index.ts
@@ -0,0 +1,5 @@
+import { ForbiddenPageAsync } from './UI/ForbiddenPage.async';
+
+export {
+    ForbiddenPageAsync as ForbiddenPage,
+};
