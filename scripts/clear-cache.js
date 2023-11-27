983faca08b11aadb664253c1d3d8ddea4fcd6d35
OlyaSpiridonova <Spiridonova.olya14@gmail.com>
2023-11-27 17:45:08 +0700

    upgrade webpack config, add babel cache

---
+++ scripts/clear-cache.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathToCache = path.resolve(__dirname, '..', 'node_modules', '.cache');
+
+fs.rm(pathToCache, { recursive: true, force: true }, (err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('Cache folder removed successfully');
+});
